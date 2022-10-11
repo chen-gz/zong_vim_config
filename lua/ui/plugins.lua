7dbdb52a533b83a5b96931b55018245eb0943bbd
Guangzong Chen <guangzong@pitt.edu>
2022-10-11 10:44:45 -0400

    add pretty fold and fold preview

--- lua/ui/plugins.lua
+++ lua/ui/plugins.lua
@@ -23,7 +23,7 @@ use {
 
 use {
     'feline-nvim/feline.nvim', -- status bar line
-    config= config.feline
+    config = config.feline
 }
 use {
     'j-hui/fidget.nvim', -- for nvim-lsp progress
@@ -36,4 +36,25 @@ use {
     'andymass/vim-matchup', -- for % and other matching
     config = config.vim_matchup
 
+}
+use {
+    'anuvyklack/pretty-fold.nvim',
+    config = config.pretty_fold
+}
+use { 'anuvyklack/fold-preview.nvim',
+    requires = 'anuvyklack/keymap-amend.nvim',
+    config = function()
+        require('fold-preview').setup()
+        local keymap = vim.keymap
+        keymap.amend = require('keymap-amend')
+        local map = require('fold-preview').mapping
+
+        keymap.amend('n', 'h', map.show_close_preview_open_fold)
+        keymap.amend('n', 'l', map.close_preview_open_fold)
+        keymap.amend('n', 'zo', map.close_preview)
+        keymap.amend('n', 'zO', map.close_preview)
+        keymap.amend('n', 'zc', map.close_preview_without_defer)
+        keymap.amend('n', 'zR', map.close_preview)
+        keymap.amend('n', 'zM', map.close_preview_without_defer)
+    end
 }
