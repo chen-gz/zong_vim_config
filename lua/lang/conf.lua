7b2057f51b9afe84f41e9c970c8b2a8d6556cd4e
Guangzong Chen <guangzong@pitt.edu>
2022-10-19 16:54:39 -0400

    update

--- lua/lang/conf.lua
+++ lua/lang/conf.lua
@@ -53,6 +53,7 @@ function config.lspconfig()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.offsetEncoding = { "utf-16" }
     require('lspconfig').clangd.setup({ capabilities = capabilities })
+    require('lspconfig').jsonls.setup {}
 
 
 end
